56d7d3f5ddf637470ef2a30448e1a06d0e5920b6
Shamimul Alam <shamimul.adndigitalbd@gmail.com>
2019-01-20 16:53:35 +0600

    Logout implementation complete

--- resources/views/layouts/adminlte.blade.php
+++ resources/views/layouts/adminlte.blade.php
@@ -206,10 +206,16 @@ scratch. This page gets rid of all links and provides the needed markup only.
                         </router-link>
                     </li>
                     <li class="nav-item">
-                        <a href="#" class="nav-link">
-                            <i class="fas fa-power-off"></i>
-                            <p>Logout</p>
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            <i class="fas fa-power-off" ></i>
+                            <p>{{ __('Logout') }}</p>
                         </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
                 </ul>
             </nav>
